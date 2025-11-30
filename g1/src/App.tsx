95179a9ae68f3d4ec2947f2f8c4c4dc741e9639c
sinay135 <sinay135@mymail.unisa.edu.au>
2025-11-30 17:57:46 +1030

    create tree

--- g1/src/App.tsx
+++ g1/src/App.tsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Summon from './components/Summon'
 import type Particle from './components/scripts/Particle';
+import QuadTree from './components/scripts/QuadTree';
 
 export default function App() {
-
-  const [particles, setParticles] = useState<Particle[]>([]);
+  const mapSize: number = 500;
+  const qt: QuadTree = new QuadTree(mapSize)
 
   return (
     <>
       <div className='world' >
         <div className='verse'>
-          <svg width={500} height={500}>
+          <svg width={mapSize} height={mapSize}>
             <g fill="grey" stroke="" stroke-width="5">
               <circle cx="500" cy="0" r="10" />
               <circle cx="0" cy="500" r="10" />
             </g>
           </svg>
         </div>
-        <Summon size={500} setParticles={setParticles} />
+        <Summon size={mapSize} qt={qt}/>
       </div>
     </>
   )
--- g1/src/components/Summon.tsx
+++ g1/src/components/Summon.tsx
@@ -1,18 +1,27 @@
+import { useEffect } from "react";
 import Particle from "./scripts/Particle"
+import type QuadTree from "./scripts/QuadTree";
 
 
 function randInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default function Summon(props: {size: number, setParticles: any}) {    
-    const quantity: number = 10;
-    const mass: number = 10;
-    for (let i = 0; i < quantity; i++) {
-        const x: number = randInt(0, props.size);
-        const y: number = randInt(0, props.size);
-        props.setParticles((e: Particle[]) => e.push(new Particle(x, y, mass)));
-    }
+export default function Summon(props: {size: number, qt: QuadTree}) {    
+    useEffect(() => {
+        const quantity: number = 10;
+        const mass: number = 10;
+        let particles: Particle[] = [];
+
+        for (let i = 0; i < quantity; i++) {
+            const x: number = randInt(0, props.size);
+            const y: number = randInt(0, props.size);
+            particles.push(new Particle(x, y, mass));
+        }
+
+        props.qt.createTree(particles);
+    })
+    
 
     return (
         <button style={{marginTop: "10px"}}>Yes</button>
